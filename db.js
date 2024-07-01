ff30a27781e7c3e8a8aa038984139bcc1572b937
Julibarrera2 <164010054+Julibarrera2@users.noreply.github.com>
2024-07-01 13:40:56 -0300

    cambie nombre database

--- db.js
+++ db.js
@@ -4,5 +4,5 @@ export const conn = await mysql.createConnection({
     user : 'root',
     password: 'rootroot',
     host: 'localhost',
-    database: 'NombreDeBaseDeDatos'
+    database: 'tp4'
 });
